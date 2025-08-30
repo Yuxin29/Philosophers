91b1c71a07f2988386d665d9330991f5caaebd7d
Yuxin29 <yuxin.1.wu@gmail.com>
2025-08-30 13:14:17 +0300

    thinking, eating and sleeping in messed up order

--- src/routine.c
+++ src/routine.c
@@ -23,8 +23,37 @@ static void routine_eat(t_philo *philo)
     printf("The philospher %d is eating\n", philo->id);
     pthread_mutex_unlock(&philo->table->forks[l]);
     pthread_mutex_unlock(&philo->table->forks[r]);
+    philo->meals_eaten += 1;
     pthread_mutex_unlock(&philo->table->printf_lock);
 
+
+}
+static void routine_sleep(t_philo *philo)
+{
+    uint64_t    end_time;
+    
+    pthread_mutex_lock(&philo->table->printf_lock);
+    printf("The starting time of the table is %lu\n", philo->table->starting_time_ms);
+    printf("Philosopher %d, is sleeping\n", philo->id + 1);
+
+    end_time = ft_to_sleep(philo->table, philo); 
+
+    printf("after the sleep, the time is %lu\n", end_time);
+    pthread_mutex_unlock(&philo->table->printf_lock);
+}
+
+static void routine_think(t_philo *philo)
+{
+    uint64_t    end_time;
+
+    pthread_mutex_lock(&philo->table->printf_lock);
+    printf("The starting time of the table is %lu\n", philo->table->starting_time_ms);
+    printf("Philosopher %d, is thinking\n", philo->id + 1);
+
+    end_time = ft_to_sleep(philo->table, philo); 
+
+    printf("after the thinking, the time is %lu\n", end_time);
+    pthread_mutex_unlock(&philo->table->printf_lock);
 }
 
 // for testing
@@ -36,24 +65,16 @@ static void routine_eat(t_philo *philo)
 void *routine(void *arg)
 {
     t_philo     *philo;
-    uint64_t    end_time;
 
     philo = (t_philo *)arg;
 
-    //check if it is dead
-
-    //sleep
-    pthread_mutex_lock(&philo->table->printf_lock);
-    printf("The starting time of the table is %lu\n", philo->table->starting_time_ms);
-    printf("Philosopher %d, is sleeping\n", philo->id + 1);
-    end_time = ft_to_sleep(philo->table, philo); 
-    printf("after the sleep, the time is %lu\n", end_time);
-    pthread_mutex_unlock(&philo->table->printf_lock);
-
-    //eat
-    routine_eat(philo);
-
-    //think
+    while (philo->meals_eaten < 3)
+    //later, this should be checking if it is dead
+    {
+        routine_eat(philo);
+        routine_think(philo);
+        routine_sleep(philo);
+    }
 
     return (NULL);
 }

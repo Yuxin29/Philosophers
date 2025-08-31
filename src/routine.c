1ec3e1c6f6bea61234fbb86a7ccfeaa3172813aa
Yuxin Wu <yuwu@c2r6p7.hive.fi>
2025-08-31 17:56:58 +0300

    done and dead checked

--- src/routine.c
+++ src/routine.c
@@ -20,19 +20,34 @@ static int	precheck_stop(t_philo *philo)
 static void	routine_eat(t_philo *philo)
 {
 	uint64_t	timestamp;
+    int first;
+    int second;
 
-	if (precheck_stop(philo))
+    if (precheck_stop(philo))
 		return;
-	if (philo->id % 2 == 0)
-	{
-		pthread_mutex_lock(&philo->table->forks[philo->fork_l]);
-		pthread_mutex_lock(&philo->table->forks[philo->fork_r]);
-	}
-	else
-	{
-		pthread_mutex_lock(&philo->table->forks[philo->fork_r]);
-		pthread_mutex_lock(&philo->table->forks[philo->fork_l]);
-	}
+    if (philo->fork_l < philo->fork_r)
+    {
+        first = philo->fork_l;
+        second = philo->fork_r;
+    }
+    else
+    {
+        first = philo->fork_r;
+        second = philo->fork_l;
+    }
+
+    pthread_mutex_lock(&philo->table->forks[first]);
+    pthread_mutex_lock(&philo->table->forks[second]);
+	// if (philo->id % 2 == 0)
+	// {
+	// 	pthread_mutex_lock(&philo->table->forks[philo->fork_l]);
+	// 	pthread_mutex_lock(&philo->table->forks[philo->fork_r]);
+	// }
+	// else
+	// {
+	// 	pthread_mutex_lock(&philo->table->forks[philo->fork_r]);
+	// 	pthread_mutex_lock(&philo->table->forks[philo->fork_l]);
+	// }
 	pthread_mutex_lock(&philo->table->state_lock);
 	philo->last_eating_time = now_ms();
 	philo->meals_eaten += 1;
@@ -42,8 +57,8 @@ static void	routine_eat(t_philo *philo)
 	printf("%lu, %d is eating\n", timestamp, philo->id + 1);
 	pthread_mutex_unlock(&philo->table->printf_lock);
 	ft_to_eat(philo->table);
-	pthread_mutex_unlock(&philo->table->forks[philo->fork_l]);
-	pthread_mutex_unlock(&philo->table->forks[philo->fork_r]);
+	pthread_mutex_unlock(&philo->table->forks[second]);
+    pthread_mutex_unlock(&philo->table->forks[first]);
 }
 
 static void	routine_sleep(t_philo *philo)
@@ -79,13 +94,14 @@ static void	routine_think(t_philo *philo)
 // eat
 // think
 // sleep
+// usleep(1000); 1 ms
 void	*routine(void *arg)
 {
 	t_philo		*philo;
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2)
-		usleep(1000);
+		usleep(500);
 	while (1)
 	{
 		if (precheck_stop(philo))
@@ -97,6 +113,7 @@ void	*routine(void *arg)
         if (precheck_stop(philo))
             break;
 		routine_think(philo);
+        usleep(50);
 	}
 	return (NULL);
 }
